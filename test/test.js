dff592943178d7a7ab5aa39408641ed453a814a8
Roberto Sanchez <roberto@digitalbrands.com>
2016-06-24 20:32:46 +0000

    project

---
+++ test/test.js
@@ -0,0 +1,14 @@
+var request = require('supertest');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('hello world');
+});
+
+describe( "TESTING RESPONSE", function() {
+	it ( "GETTING RESPONSE", function(done) {
+		request(app).get('/').expect( 'hello world', done );
+	});
+});
